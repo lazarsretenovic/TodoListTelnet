900a1e0f09082654af3f59983e9ed5cb318fc83d
dev-ex000 <lazar.sretenovic@dev.telnet.rs>
2023-10-02 10:40:19 +0200

    Created 2 entity classes for user and todolist, created getters,setters, constructors and annotated them with jpa annotations

---
+++ TodoList/src/main/java/domain/Todo.java
@@ -0,0 +1,65 @@
+package domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "status")
+    private boolean status;
+    private User user;
+
+    public Todo() {
+    }
+
+    public Todo(String description, boolean status) {
+        this.description = description;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
